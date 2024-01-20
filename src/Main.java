8178d5b01003d004485fbdfcecbc81cbe8de2d48
yeshwant <yeshwantkumar03@gmail.com>
2024-01-20 14:33:28 +0530

    Car car3 = new Car("C003","Lamborghini","avantador","150.0");         Added one more object

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,12 @@ public class Main {
         Car car1 = new Car("C001","Tesla","compact sedan","60.0");
         Car car2 = new Car("C002","Ferrari","Portofino","70.0");
         Car car3 = new Car("C003","Lamborghini","avantador","150.0");
+        Car car4 = new Car("C004","Audi","avant","180.0");
+
         carArrayList.add(car1);
         carArrayList.add(car2);
         carArrayList.add(car3);
+        carArrayList.add(car4);
 
 
 
